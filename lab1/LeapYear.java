51938f6c19d4de46185023e9c58c5c4930aaa9c8
Ke <krypton.yang@gmail.com>
2021-03-10 14:09:57 -0500

    finish lab1 Leapyear

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -6,6 +6,19 @@ public class LeapYear {
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
+    /** return true if year is divided by 4 or 400, but not 100*/
+    public static boolean isLeapYear(int year) {
+        if(year%400 == 0) {
+            return true;
+        }
+        else{
+            if (year % 4 == 0 & year % 100 != 0) {
+
+                return true;
+            }
+        }
+        return false;
+    }
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
             System.out.printf("%d is a leap year.\n", year);
