48e3aa8b201deb3596b5d89432386b4a97953364
Ke <krypton.yang@gmail.com>
2021-04-16 17:02:17 -0400

    finish of working neighbours

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -2,6 +2,9 @@ package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Percolation {
     /**create N by N grid, with all sites initially blocked */
     int[] mp;
@@ -19,17 +22,37 @@ public class Percolation {
     }
     /**open the site(row, col) if it is not open already*/
     public void open(int row, int col){
-
-        if(mp[convert(row, col)] == 0){
+        int currentspot = convert(row, col);
+        if(mp[currentspot] == 0){
             opensite += 1; //not yet finish
-
+            
         }
 
 
     }
-    //private method check if the surrouding of this spot is opened
-    private boolean isLinked(int row, int col){
+    //private method return a list of neighbours position
+    //every spot can have 2,3,4 neighbours
+    private List<Integer> neighbours(int row, int col){
 
+        List neighbourList = new ArrayList();
+        int currentspot = convert(row, col);
+        int[] updownNeighbours = new int[]{currentspot - n, currentspot + n};
+        for(int i = 0; i< 2; i++){
+            if(updownNeighbours[i]>= 0 && updownNeighbours[i] <= n*n-1 ){
+                neighbourList.add(updownNeighbours[i]);
+            }
+        }
+        if(col == 0){
+            neighbourList.add(currentspot + 1);
+        }
+        else if(col == n - 1){
+            neighbourList.add(currentspot - 1);
+        }
+        else{
+            neighbourList.add(currentspot + 1);
+            neighbourList.add(currentspot - 1);
+        }
+            return neighbourList;
     }
     //covert a two d position into one d
     private int convert(int row, int col){
@@ -39,7 +62,7 @@ public class Percolation {
     public boolean isOpen(int row, int col){
         return mp[convert(row, col)] != 0;
     }
-    /**is the site full?*/
+    /**is the site full?*///is connected to top?(array position 0~N-1
     public boolean isFull(int row, int col){
         return true;
     }
@@ -47,12 +70,17 @@ public class Percolation {
     public int numberofOpenSites(){
         return opensite;
     }
-    /**does the system percolate*/
+    /**does the system percolate*/ //if any of the top(array position 0~N-1) is connected
+    //with any of the bottom (array position N*(N-1)~N*N - 1
     public boolean percolates(){
         return true;
     }
     /**unit testing*/
-    public static void mian(String[] args){
+    public static void main(String[] args){
+        Percolation p = new Percolation(6);
+        List<Integer> a = p.neighbours(2,5);
+        for(int i : a){
+        System.out.println(i);}
 
     }
 
